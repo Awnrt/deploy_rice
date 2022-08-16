f691790ee5c5d3d343880ce0cd11e13c6fff825a
Awnrt <74102659+Awnrt@users.noreply.github.com>
2022-08-16 02:58:50 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 sudo pacman -Sy
 sudo pacman -Sy git xorg-server xorg-xrandr xorg-xrdb feh xorg-xinput xorg-xinit libx11 libxinerama libxft webkit2gtk conky picom lxappearance alsa-utils pulseaudio pulsemixer
 sudo cp undefined-medium.ttf /usr/share/fonts/ 
+cd /home/$USER/
 git clone https://github.com/Awnrt/dwm
 cd dwm
 sudo make clean install
@@ -12,18 +13,18 @@ cd ..
 git clone https://github.com/Awnrt/dmenu
 cd dmenu
 sudo make clean install
-cd ..
+cd deploy_rice
 echo "startx" >> /home/jimbob/.bash_profile
 mv gtkrc-2.0 .gtkrc-2.0
-cp .gtkrc-2.0 ~/
+cp .gtkrc-2.0 /home/$USER/
 mv xinitrc .xinitrc
-cp .xinitrc ~/
-cp -r gtk-3.0 ~/.config/
-cp picom.conf ~/.config/picom/
+cp .xinitrc /home/$USER/
+cp -r gtk-3.0 /home/$USER/.config/
+cp picom.conf /home/$USER/.config/picom/
 mv Xresources .Xresources
-cp .Xresources ~/
+cp .Xresources /home/$USER/
 mv conkyrc .conkyrc
-cp .conkyrc ~/
-cp -r wallpapers ~/
+cp .conkyrc /home/$USER/
+cp -r wallpapers /home/$USER/
 sudo cp -r macOSBigSur /usr/share/icons/
 sudo cp -r gtk-master /usr/share/themes/
