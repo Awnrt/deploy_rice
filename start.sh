c9ac902de385894797302078fd28eb2b7065187c
Awnrt <74102659+Awnrt@users.noreply.github.com>
2022-08-16 03:05:55 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 sudo pacman -Sy
 sudo pacman -Sy git xorg-server xorg-xrandr xorg-xrdb feh xorg-xinput xorg-xinit libx11 libxinerama libxft webkit2gtk conky picom lxappearance alsa-utils pulseaudio pulsemixer
 sudo cp undefined-medium.ttf /usr/share/fonts/ 
-cd /home/$USER/
+cd /home/awy/
 git clone https://github.com/Awnrt/dwm
 cd dwm
 sudo make clean install
@@ -13,18 +13,18 @@ cd ..
 git clone https://github.com/Awnrt/dmenu
 cd dmenu
 sudo make clean install
-cd /home/$USER/deploy_rice
-echo "startx" >> /home/$USER/.bash_profile
+cd /home/awy/deploy_rice
+echo "startx" >> /home/awy/.bash_profile
 mv gtkrc-2.0 .gtkrc-2.0
-cp .gtkrc-2.0 /home/$USER/
+cp .gtkrc-2.0 /home/awy/
 mv xinitrc .xinitrc
-cp .xinitrc /home/$USER/
-cp -r gtk-3.0 /home/$USER/.config/
-cp picom.conf /home/$USER/.config/picom/
+cp .xinitrc /home/awy/
+cp -r gtk-3.0 /home/awy/.config/
+cp picom.conf /home/awy/.config/picom/
 mv Xresources .Xresources
-cp .Xresources /home/$USER/
+cp .Xresources /home/awy/
 mv conkyrc .conkyrc
-cp .conkyrc /home/$USER/
-cp -r wallpapers /home/$USER/
+cp .conkyrc /home/awy/
+cp -r wallpapers /home/awy/
 sudo cp -r macOSBigSur /usr/share/icons/
 sudo cp -r gtk-master /usr/share/themes/
